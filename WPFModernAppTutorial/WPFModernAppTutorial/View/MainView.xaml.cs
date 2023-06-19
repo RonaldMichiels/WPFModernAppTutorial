b7d6d98372c6266b94457a779c10323815a55c06
Ronald Michiels <ronald.michiels@inetum-realdolmen.world>
2023-06-19 12:14:45 +0200

    Added Styling for mainwindow and left menu

--- WPFModernAppTutorial/WPFModernAppTutorial/View/MainView.xaml.cs
+++ WPFModernAppTutorial/WPFModernAppTutorial/View/MainView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace WPFModernAppTutorial.View
 {
@@ -22,6 +24,20 @@ namespace WPFModernAppTutorial.View
         public MainView()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        private void pnlContorlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void pnlContorlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
